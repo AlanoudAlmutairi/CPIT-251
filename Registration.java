0d4cff0157482ca321895bd5373cc1dc55f24a0d
Alanoud_Almutairi <127936079+AlanoudAlmutairi@users.noreply.github.com>
2023-05-17 08:06:14 +0300

    Add files via upload

--- Registration.java
+++ Registration.java
@@ -10,4 +10,47 @@ package pkg251project;
  */
 public class Registration {
     
+    private String Registration_code ;
+    private String Event_name ;
+    private String Volunteer_Role ;
+    private String Volunteer_name ;
+    
+  public Registration (String Registration_code,String Event_name , String Volunteer_Role,String Volunteer_name){
+      this.Registration_code = Registration_code ;
+      this.Event_name = Event_name ;
+      this.Volunteer_Role = Volunteer_Role ;
+      this.Volunteer_name = Volunteer_name ;
+  }
+  
+  public String GetRegistration_code(){
+      return Registration_code ;
+}
+  public void SetRegistration_code(String code ){
+      this.Registration_code = code ;
+  }
+  public String GetEvent_name(){
+      return Event_name ;
+  }
+  public void SetEvent_name(String eventName){
+      this.Event_name = eventName ;
+  }
+  public String GetVolunteer_Role(){
+      return Volunteer_Role ;
+  }
+  public void SetVolunteer_Role( String Role){
+      this.Volunteer_Role = Role;
+  }
+  public String GetVolunteer_name(){
+      return Volunteer_name ;
+  }
+  public void SetVolunteer_name( String volunteerName ){
+      this.Volunteer_name = volunteerName ;
+  }
+  
+  public String Display_Ads_info(){
+  
+  return " ";
+  }
+  
+  
 }
