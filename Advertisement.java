b60d07f8c5385bdcaf65cf6846157addb4bac50b
Alanoud_Almutairi <127936079+AlanoudAlmutairi@users.noreply.github.com>
2023-05-17 08:05:51 +0300

    Add files via upload

--- Advertisement.java
+++ Advertisement.java
@@ -10,4 +10,31 @@ package pkg251project;
  */
 public class Advertisement {
     
+    private String event_specifications;
+    private String Advertisement_code ;
+    private String Advertisement_scope ;
+    
+   public Advertisement(String event_specifications ,String Advertisement_code,String Advertisement_scope) {
+       this.event_specifications = event_specifications;
+       this.Advertisement_code = Advertisement_code ;
+       this.Advertisement_scope = Advertisement_scope ;
+   }
+   public String Getevent_specifications(){
+       return event_specifications ;
+   }
+   public void Setevent_specifications( String specification ){
+       this.event_specifications = specification ;
+   }
+   public String GetAdvertisement_code(){
+       return Advertisement_code;
+   } 
+   public void setAdvertisement_code( String code ){
+       this.Advertisement_code = Advertisement_code;
+   }
+   public String GetAdvertisement_scope(){
+       return Advertisement_scope;
+   }
+   public void setAdvertisement_scope( String scope){
+       this.Advertisement_scope = scope ;
+}
 }
