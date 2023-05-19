432577302f89655520eae77dba986f39438637bf
Alanoud_Almutairi <127936079+AlanoudAlmutairi@users.noreply.github.com>
2023-05-19 10:18:24 +0300

    Add files via upload

---
+++ special_needs_helpre.java
@@ -0,0 +1,26 @@
+
+public class special_needs_helpre {
+    public boolean dealing_with_specia_needs_exp;
+    
+    public special_needs_helpre() {
+    }
+
+    public boolean isDealing_with_specia_needs_exp() {
+        return dealing_with_specia_needs_exp;
+    }
+
+    public void setDealing_with_specia_needs_exp(boolean dealing_with_specia_needs_exp) {
+        this.dealing_with_specia_needs_exp = dealing_with_specia_needs_exp;
+    }
+
+    
+
+    
+    @Override
+    public String toString() {
+        return "special_needs_helpre{" + "dealing_with_specia_needs_exp=" + dealing_with_specia_needs_exp + '}';
+    }
+    
+    
+    
+}
