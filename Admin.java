c8d0abbe2d2ea29ff19f3f0993053c107e9fd35b
Alanoud_Almutairi <127936079+AlanoudAlmutairi@users.noreply.github.com>
2023-05-20 19:19:17 +0300

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,42 @@
+
+public class Admin {
+    private int length_of_service;
+    private int rank_of_adminstration;
+
+    public Admin(int length_of_service, int rank_of_adminstration) {
+        this.length_of_service = length_of_service;
+        this.rank_of_adminstration = rank_of_adminstration;
+    }
+
+    public int getLength_of_service() {
+        return length_of_service;
+    }
+
+    public void setLength_of_service(int length_of_service) {
+        this.length_of_service = length_of_service;
+    }
+
+    public int getRank_of_adminstration() {
+        return rank_of_adminstration;
+    }
+
+    public void setRank_of_adminstration(int rank_of_adminstration) {
+        this.rank_of_adminstration = rank_of_adminstration;
+    }
+    public void add_advertisements(){
+        
+    }
+    public void delet_addvertisements(){
+        
+    }
+    public void categorization_addvertisements(){
+        
+    }
+    public void display_addvertisement(){
+        
+    }
+    public void provide_leadership_on_issues(){
+        
+    }
+}
+
