3394203cf24a827ce3177d2b559fcf59a2c51a24
MDM <128228581+MulookAl@users.noreply.github.com>
2023-05-24 17:04:41 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -43,17 +43,27 @@ public class Main {
     
     
     public static void Admin(Admin a1 , Scanner input){
-      //a1.log_in();
-      
-      System.out.println("select:");
-      String select = input.next();
-      do{
-      if(select.equalsIgnoreCase("A"))
-         a1.add_advertisements();
-        System.out.println();
-         
-          
-          }while(select=="q"); 
+           String adv;
+        char service = 'a';
+        do {
+         System.out.println("Enter a for Add , Enter d for Delete , q for quit");
+         service = input.next().charAt(0);
+        if (service == 'a'){
+            adv = a1.add_advertisements(Adv,advCounter);
+            System.out.println(adv);
+            System.out.println(" event_specifications: " + Adv.get(advCounter).Getevent_specifications()
+                + " Advertisement_code: "+Adv.get(advCounter).GetAdvertisement_code() + " Advertisement_scope: " 
+                + Adv.get(advCounter).GetAdvertisement_scope());
+        }
+        else if (service== 'd'){
+            System.out.println("enter the code of add");
+            String s = input.next();
+            a1.delet_addvertisements(Adv,s);
+            
+        }
+        }while(service != 'q');
+                  
+    }
     }
     
 }
