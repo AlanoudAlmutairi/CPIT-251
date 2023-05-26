027ccbc541a296a97a6796a926ba1ea672651c6f
Alanoud_Almutairi <127936079+AlanoudAlmutairi@users.noreply.github.com>
2023-05-26 10:04:42 +0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -23,12 +23,47 @@ public class Admin {
     public void setRank_of_adminstration(int rank_of_adminstration) {
         this.rank_of_adminstration = rank_of_adminstration;
     }
-    public void add_advertisements(){
-        
+  
+     public String add_advertisements( ArrayList<Advertisement> Adv, int index ){
+     System.out.print("Enter event specification: ");
+     String event_specifications = input.next();
+     System.out.print("Enter Advertisement code :");
+     String Advertisement_code = input.next();
+     System.out.print("Enter Advertisement scope :");
+     String Advertisement_scope=input.next();
+     Advertisement AA=new Advertisement(event_specifications,Advertisement_code, Advertisement_scope);
+     Adv.add(AA);
+     index++;
+     return "Suceess";
     }
-    public void delet_addvertisements(){
+    
+    
+       public int searchAdv(ArrayList<Advertisement> Adv , String Advertisement_code  ){
+       for(int index = 0 ; index < Adv.size() ; index++){
+            if (Advertisement_code.equals(Adv.get(index).GetAdvertisement_code()) ){
+                return index ;
+                
+            }
+            
+       } 
+     return -1;   
+    }    
+    
+   public void delet_addvertisements(ArrayList<Advertisement> Adv , String Advertisement_code){
+       
+        int pointer2delet= searchAdv( Adv ,  Advertisement_code  );
         
+        if (pointer2delet == -1 ){
+            System.out.println("This "+Advertisement_code +" doesn't exists");
+        }
+        else{
+            Adv.remove(Adv.get(pointer2delet));
+            
+            System.out.println("Deleted successfully");
+        }
     }
+    
+    
     public void categorization_addvertisements(){
         
     }
