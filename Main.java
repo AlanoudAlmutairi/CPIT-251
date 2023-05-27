79764a8e50efdc064f174fdfb78a4bfcd48f5195
Erada_Alqadi <127771422+erer21@users.noreply.github.com>
2023-05-27 19:48:13 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
 /**
  *
  * @author alano
@@ -52,7 +47,7 @@ public class Main {
                    break;
            }
   
-    }while(Role!='e');   
+      }while(Role!='e');   
     } 
     
     
